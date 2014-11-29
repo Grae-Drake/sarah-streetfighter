c51a60d7de5fdcf400b7f48637e1bf9ac6f0bf08
Grae Drake <graemdrake@gmail.com>
2014-11-29 17:07:24 -0500

    styling for HTML, CSS and JS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
+
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
+  });
+
+  $('.ryu').mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
-  })
-  .mousedown(function() {
+  });
+
+  $('.ryu').mousedown(function() {
     console.log('mousedown');
     playHadouken();
     $('.ryu-ready').hide();
@@ -21,10 +24,30 @@ $(document).ready(function() {
         $(this).css('left', '-212px');
       }
     );
-  })
-  .mouseup(function() {
+  });
+
+  $('.ryu').mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
+  });
+
+  $('.ryu').keydown(function(e) {
+    console.log('keydown');
+    if(e.keyCode == 88){
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.hadouken').hide();
+      $('.ryu-cool').show();
+    }
+  });
+
+  $('.ryu').keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-still').show();
+      $('ryu-ready').hide();
+      $('.hadouken').hide();
+      $('ryu-cool').hide();
+    }
   });
 });
 
@@ -33,22 +56,3 @@ function playHadouken () {
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-$(document).ready(function(){
-    $('.ryu').keydown(function(e) {
-      console.log('keydown');
-        if(e.keyCode == 88){
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').hide();
-            $('.hadouken').hide();
-            $('.ryu-cool').show();
-        }
-    });
-    $('.ryu').keyup(function(e) {
-        if (e.keyCode == 88) {
-            $('.ryu-still').show();
-            $('ryu-ready').hide();
-            $('.hadouken').hide();
-            $('ryu-cool').hide();
-        }
-    });
-});
